d470613ea8955754a83044f064bb25b75a728f4d
CS70 koala <xiyang@hmc.edu>
2018-04-12 22:01:16 -0700

    create dir

--- get_peopleurl.py
+++ get_peopleurl.py
@@ -4,5 +4,132 @@ import requests
 from urllib.request import urlopen
 from urllib.parse import urljoin
 import http.cookiejar
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bsoup
+import re
+import os
 
+def next_url(href):
+    return href and re.compile("subcatfrom").search(href)
+
+def oldid(href):
+    return href and re.compile("oldid").search(href)
+
+
+    
+    
+# badlink = [
+#     /index.php?title=Category:People&subcatfrom=Aeschbacher%2C+Karl#mw-subcategories
+# http://imslp.org/index.php?title=Category:People&oldid=2523965
+# /wiki/Special:GetFCtrStats/=Category%3APeople
+# /wiki/Special:SwitchSkin/vector/Category%3APeople
+# ]
+# loop through the links and get peope url
+
+def find_next_url(link):
+    """
+    find the sub-pages for all people list
+    """
+    url = link
+    result = []
+    while True: 
+        r = requests.get(url)
+        soup = bsoup(r.text, "html.parser")
+        url_list = [link.get('href') for link in soup.find_all(href=next_url)]
+        url = 'http://imslp.org' + url_list[-1]
+        print(url)
+        result.append(url)
+    return result
+
+def find_people_url(url):
+    r = requests.get(url)
+    soup = bsoup(r.text, "html.parser")
+    url_list = [link.get('href') for link in soup.find_all('a',class_='categorysubcatlink')]
+    return url_list
+
+def write_people_url():
+    result = []
+    for url in open('results/people_page_url.txt'):
+        result = result + find_people_url(url)
+    text = open("result.txt","w")
+    for item in result:
+        item = 'http://imslp.org'+item
+        text.write("%s\n" % item)
+        # TODO: add Id using oldid function
+    text.close()
+    print("total number of composer: ", len(result))
+
+def find_next_score_url(url):
+    """
+    find the sub-page for score list
+    """
+    result = []
+    while True:
+        r = requests.get(url)
+        soup = bsoup(r.text, "html.parser")
+        url_list = [link.get('href') for link in soup.find_all('a',class_='categorypaginglink',string='next 200')]
+        if url_list == []:
+            break
+        url = 'http://imslp.org' + url_list[-1]
+        result.append(url)
+    return result
+
+def find_score_url(url):
+    list_page = [url]
+    r = requests.get(url)
+    soup = bsoup(r.text, "html.parser")
+    # add everything on the first page to the list
+    result = soup.find_all('div', class_="jq-ui-tabs" )
+    scores = result[0].find_all('a',class_='categorypagelink')
+    url_list = [link.get('href') for link in scores]
+    # check sub-page
+    list_page = find_next_score_url(url)
+    if list_page != []:
+        for page in list_page:
+            r = requests.get(page)
+            soup = bsoup(r.text, "html.parser")
+            url_list += [link.get('href') for link in soup.find_all('a',class_='categorypagelink')]
+    return url_list
+
+def write_score_url():
+    text = open("result.txt","w")
+    for url in open('results/composer_url.txt'):
+        result = []
+        print(url)
+        result = find_score_url(url)
+        for item in result:
+            item = 'http://imslp.org'+item
+            text.write("%s\n" % item)
+        # TODO: add Id using oldid function
+    text.close()
+
+def create_composer_dir():
+    for url in open('results/composer_url.txt'):
+        parent = '/Users/CarrieYang/Desktop/score_scrape/composer/'+url[31:-1]
+        print('write into: ', parent)
+        score_links = find_score_url(url)
+        for url in score_links:
+            url = url[6:url.find('(')]
+            path = parent+'/'+url
+            print('final dir:',path)
+            os.makedirs(path, exist_ok=True)
+
+            name_of_file = raw_input("html")
+            completeName = os.path.join(path, name_of_file+".txt")         
+            file1 = open(completeName, "w")
+            r = requests.get(url)
+            toFile = raw_input(r.text)
+            file1.write(toFile)
+            file1.close()
+
+
+# def create_piece_dir():
+#     for url in open('results/composer_url.txt'):
+#         path = '/Users/CarrieYang/Desktop/score_scrape/composer/'+url[31:-1]+
+
+
+
+if __name__=='__main__':
+    # # find all the links for different pages
+    # link = 'http://imslp.org/wiki/Category:Composers'
+    # find_next_url(link)
+    create_composer_dir()
---
+++ metadata.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
+
+html = urllib.request.urlopen('http://www.nytimes.com/2009/12/21/us/21storm.html').read()
+print(text_from_html(html))
+print(text[text.find('Editor'):text.find('Categories:'])
