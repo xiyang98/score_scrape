27ea93fa3b61bdcfc8038c3b1e122d3e0a032b46
CS70 koala <xiyang@hmc.edu>
2018-04-12 22:16:43 -0700

    update dir

--- get_peopleurl.py
+++ get_peopleurl.py
@@ -112,7 +112,8 @@ def create_composer_dir():
             piecename = url[6:url.find('(')]
             path = os.path.join(parent, piecename)
             print('final dir:',path)
-            os.makedirs(path, exist_ok=True)
+            # os.makedirs(path, exist_ok=True)
+            os.makedirs(path)
 
             completeName = os.path.join(path, "html.txt")         
             file1 = open(completeName, "w")
